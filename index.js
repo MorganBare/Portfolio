e71053758ebbe42e86f9040c63493f63eac35345
MorganBare <morganjbare@gmail.com>
2022-03-08 19:52:30 +0100

    Hamburger animation added and basic mobile menu sliding from the right

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-$(".toggle-icon").click(function() {
-    $('#nav-container').toggleClass("pushed");
-  });
+const hamburger = document.getElementById("hamburger");
+const nav = document.getElementById("overlay");
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar3 = document.getElementById('bar3');
+const overlay = document.getElementById('overlay')
+
+
+hamburger.addEventListener("click", function(){
+    console.log('click')
+    overlay.classList.toggle('open');
+    bar1.classList.toggle('bar1Animate');
+    bar2.classList.toggle('bar2Animate');
+    bar3.classList.toggle('bar3Animate');
+})
+   
+
+
+
   
-  
-  /*** starwars.com's Toggle Icon Animation ***/
