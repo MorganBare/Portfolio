b19d5e2736feb36abcdd9c54206465d6d894b01f
MorganBare <morganjbare@gmail.com>
2022-03-20 16:02:57 +0100

    Added mobile menu with background blur

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const nav = document.getElementById("overlay");
 const bar1 = document.getElementById('bar1');
 const bar2 = document.getElementById('bar2');
 const bar3 = document.getElementById('bar3');
-const overlay = document.getElementById('overlay')
+const overlay = document.getElementById('overlay');
+const main = document.getElementById('main');
+const footer = document.getElementById('footer');
 
 
 hamburger.addEventListener("click", function(){
@@ -12,6 +14,8 @@ hamburger.addEventListener("click", function(){
     bar1.classList.toggle('bar1Animate');
     bar2.classList.toggle('bar2Animate');
     bar3.classList.toggle('bar3Animate');
+    main.classList.toggle('is-blurred');
+    footer.classList.toggle('is-blurred');
 })
    
 
